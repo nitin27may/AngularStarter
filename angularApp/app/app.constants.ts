e2f7e2cc1f9f81b1e75ff79f6f73cf5582b3e8c0
Nitin Singh <nitin27may@gmail.com>
2017-06-28 00:26:11 +0530

    InitialCommit of the project
    Initial commit

---
+++ angularApp/app/app.constants.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class Configuration {
+   //public Server = 'http://localhost:9195/';
+   public Server = 'http://sampleapp.in/';
+}
