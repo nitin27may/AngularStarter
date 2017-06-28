db2bb19765dc9bf7e90d763c45562567fa18765f
Nitin Singh <nitin27may@gmail.com>
2017-06-29 00:31:53 +0530

    Initial Changes for Language Dropdown selection, UserName Remeber me

--- angularApp/app/shared/components/login/login.component.ts
+++ angularApp/app/shared/components/login/login.component.ts
@@ -1,13 +1,36 @@
-﻿import { Component, NgModule, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Router, CanActivate } from '@angular/router';
-import { APIService } from '../../../core/services/api.service';
-import { LoaderService } from '../../services/loader.service';
-import { StorageService } from '../../services/storage.service';
-import { CustomToasterService } from '../../services/custom-toaster.service';
-import { StorageType } from '../../model/storage.enum';
+﻿import {
+    Component,
+    NgModule,
+    OnInit
+} from '@angular/core';
+import {
+    FormGroup,
+    FormBuilder,
+    Validators
+} from '@angular/forms';
+import {
+    Router,
+    CanActivate
+} from '@angular/router';
+import {
+    APIService
+} from '../../../core/services/api.service';
+import {
+    LoaderService
+} from '../../services/loader.service';
+import {
+    StorageService
+} from '../../services/storage.service';
+import {
+    CustomToasterService
+} from '../../services/custom-toaster.service';
+import {
+    StorageType
+} from '../../model/storage.enum';
 
-import { TranslateService } from '@ngx-translate/core';
+import {
+    TranslateService
+} from '@ngx-translate/core';
 
 @Component({
     selector: 'login',
@@ -16,7 +39,10 @@ import { TranslateService } from '@ngx-translate/core';
 })
 
 export class LoginComponent implements OnInit {
-    public user: any = { Email: '', Password: '' };
+    public user: any = {
+        Email: '',
+        Password: ''
+    };
     loginForm: any;
     constructor(private translate: TranslateService,
         private formBuilder: FormBuilder,
@@ -36,22 +62,39 @@ export class LoginComponent implements OnInit {
         this.loaderService.changePage(true);
         this.apiService.Login(user).subscribe(
             data => {
+                if (user.RememberMe) {
+                    this.storageService.save(StorageType.local, "RememberMe", user.RememberMe);
+                    this.storageService.save(StorageType.local, "UserEmail", user.Email);
+                }else{
+                    this.storageService.remove(StorageType.local, "RememberMe");
+                    this.storageService.remove(StorageType.local, "UserEmail");
+                }
                 this.loaderService.changePage(false);
                 this.storageService.save(StorageType.session, "jwtToken", data.token);
                 this.storageService.save(StorageType.session, "userDetails", JSON.stringify(data));
                 this.router.navigate(['admin/users']);
             },
             error => {
-                this.customToasterService.addToast({ title: 'Message', msg: error._body, type: 'error' });
+                this.customToasterService.addToast({
+                    title: 'Message',
+                    msg: error._body,
+                    type: 'error'
+                });
                 this.loaderService.changePage(false);
             }
         );
     }
+    selectLanguage(lang: any) {
+        this.storageService.save(StorageType.local, "language", lang);
+        this.translate.use(lang);
+    }
     ngOnInit() {
         this.loginForm = this.formBuilder.group({
-            'Email': ['', [Validators.required, Validators.minLength(3)]],
-            'Password': ['', [Validators.required, Validators.minLength(5)]]
+            'Email': [this.storageService.get(StorageType.local, "UserEmail") ? this.storageService.get(StorageType.local, "UserEmail") : null, [Validators.required, Validators.minLength(3)]],
+            'Password': ['', [Validators.required, Validators.minLength(5)]],
+            'RememberMe': (Boolean)(this.storageService.get(StorageType.local, "RememberMe")),
+            'language': this.storageService.get(StorageType.local, "language") ? this.storageService.get(StorageType.local, "language") : null
         });
-
+        this.translate.use(this.storageService.get(StorageType.local, "language") ? this.storageService.get(StorageType.local, "language") : 'en');
     }
 }
